9271a77f08bb0b0f92fd2928e75733819c608d5c
Julian Wolfe <juliancwolfe@gmail.com>
2018-06-24 20:11:18 -0400

    Adds missing documentation and imports

--- dev/index.js
+++ dev/index.js
@@ -2,10 +2,16 @@ import Connection from './core/Connection';
 import Entity from './core/Entity';
 import Model from './core/Model';
 import Repository from './core/Repository';
+import Collection from './core/Collection';
+import Factory from './core/Factory';
+import Provider from './core/Provider';
 
 export {
     Connection, 
     Entity, 
     Model, 
-    Repository
+    Repository,
+    Collection,
+    Factory,
+    Provider,
 };
